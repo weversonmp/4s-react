f3bb70189047db08b894fe7b5a386563d948bdee
weversonmp <weversonmnzs@gmail.com>
2024-01-24 00:43:23 -0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
-import './App.css';
-import Header from './components/Header';
+const { useState } = require("react");
 
 function App() {
-  return <Header />;
+  const [input, setInput] = useState('');
+  const [tarefas, setTarefas] = useState([
+    'Pagar a conta de luz',
+    'Estudar React JS'
+  ]);
+
+  function salvarRegistro(e) {
+    e.preventDefault()
+
+    setTarefas([...tarefas, input])
+    setInput('');
+  }
+
+  return (
+    <div>
+      <h1>Cadastrando Usuário</h1>
+
+      <form onSubmit={salvarRegistro}>
+        <label>Nome da Tarefa:</label> <br />
+        <input
+          placeholder="Digite uma tarefa"
+          value={input}
+          onChange={(e) => { setInput(e.target.value) }}
+        />
+
+        <br />
+        <br />
+
+        <button type="submit">Registrar</button>
+      </form>
+      <br />
+      <br />
+
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/Nome.js
@@ -0,0 +1,9 @@
+function Nome({ aluno, idade }) {
+  return (
+    <span color="#363758">
+      Bem vindo: {aluno}, - Idade: {idade} anos
+    </span>
+  );
+}
+
+export default Nome;
